584c9fc48f67794625d8f80c7d730c4f9754df72
Klopez-2024401 <klopez-2024401@kinal.edu.gt>
2026-04-06 17:17:55 -0600

    se agregaro formulario y imagen y nuevos cambios

--- KinalSports/Client-admin/src/features/auth/components/LoginForm.jsx
+++ KinalSports/Client-admin/src/features/auth/components/LoginForm.jsx
@@ -2,6 +2,22 @@
 
 export const LoginForm = () => {
     return (
-        <div>LoginForm</div>
+        <form action="" className="space-y-S">
+            <div>
+                <label 
+                    htmlFor=""
+                    className="block text-sm font-medium text-gray-800 mb-1.5"    
+                >
+                    Email o Usuario
+                </label>
+
+                <input 
+                    id="emailOrUsername"
+                    type="text" 
+                    placeholder="correo@ejemplo.com o usuario"
+                    className="w-full px-3 py-2 text-sm border border-gray-300 rounded-lg foc"
+                />
+            </div>
+        </form>
     )
 }
